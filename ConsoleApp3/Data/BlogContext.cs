500c7804f8bf98beab667ebcbeb77e40e765bad2
Lukasz Pluta <lukasz.pluta@dxc.com>
2023-01-12 20:50:46 +0100

    Add project files.

---
+++ ConsoleApp3/Data/BlogContext.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp3.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp3.Data
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public BlogContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=localhost;TrustServerCertificate=True;User Id=EAD\lpluta;Integrated Security=true;Initial Catalog=Nowa;Connection Timeout=4;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("domyslna_nazwa_schematu");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3.Data;
+using ConsoleApp3.Models;
+
+
+Console.WriteLine("Hello, World!");
+using (var db = new BlogContext())
+{
+    db.Blogs.Add(new Blog { Name = "Another Blog " });
+    db.SaveChanges();
+
+    foreach (var blog in db.Blogs)
+    {
+        Console.WriteLine(blog.Name);
+    }
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
